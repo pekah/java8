79ef62e021fcf118dd0a0189c8aee6297ce8bc07
周亿霖 <superyilin@163.com>
2018-02-28 23:28:50 +0800

    minBy

--- src/main/java/com/eli/stream/StreamTest13.java
+++ src/main/java/com/eli/stream/StreamTest13.java
@@ -1,6 +1,7 @@
 package com.eli.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,7 +14,7 @@ public class StreamTest13 {
         Student s1 = new Student("zhangsan", 100, 19);
         Student s2 = new Student("lisi", 90, 20);
         Student s3 = new Student("wangwu", 90, 20);
-        Student s4 = new Student("zhangsan", 80, 18);
+        Student s4 = new Student("zhangsan2", 80, 18);
 
         List<Student> studentList = Arrays.asList(s1, s2, s3, s4);
 
@@ -31,9 +32,12 @@ public class StreamTest13 {
 //                .collect(Collectors.groupingBy(Student::getName, Collectors.averagingDouble(Student::getScore)));
 //        System.out.println(result);
 
-        Map<Boolean, List<Student>> result = studentList.stream()
-                .collect(Collectors.partitioningBy(student -> student.getScore() >= 90));
-        System.out.println(result);
+//        Map<Boolean, List<Student>> result = studentList.stream()
+//                .collect(Collectors.partitioningBy(student -> student.getScore() >= 90));
+//        System.out.println(result);
+
+        studentList.stream().collect(Collectors.minBy(Comparator.comparingInt(Student::getScore)))
+                .ifPresent(s -> System.out.println(s.getName()));
 
     }
 }
