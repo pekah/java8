928be344501fdebf829dbf43e0e9f4c0ad23954d
pekah <474705863@qq.com>
2019-01-08 23:23:27 +0800

    multi input function

--- src/main/java/com/eli/jdk8/MultiFunction.java
+++ src/main/java/com/eli/jdk8/MultiFunction.java
@@ -1,7 +1,12 @@
 package com.eli.jdk8;
 
+/**
+ * jdk自带只有Function接收一个参数、BiFunction接收2个参数。
+ * MultiFunction可以接收多个参数
+ * @param <T>
+ * @param <R>
+ */
 @FunctionalInterface
 public interface MultiFunction<T, R> {
-
     R apply(T... args);
 }
